9871a48144fa6d700845338b5c2ee38dd8e09bf7
Q. Price <priceqp@gmail.com>
2020-05-26 11:28:56 -0500

    complete challenge 11

--- 11_flip_case/flip_case.py
+++ 11_flip_case/flip_case.py
@@ -11,3 +11,16 @@ def flip_case(phrase, to_swap):
         'AaaaHHH'
 
     """
+
+    new_phrase = []
+
+    for letter in phrase:
+
+        if letter.lower() == to_swap.lower():
+
+            new_phrase.append(letter.swapcase())
+
+        else:
+            new_phrase.append(letter)
+
+    return "".join(new_phrase)
