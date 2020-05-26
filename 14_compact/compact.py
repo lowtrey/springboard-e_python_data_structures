9ccd6798c2dd897defd8f779fc0475f8df3f7fdb
Q. Price <priceqp@gmail.com>
2020-05-26 12:17:17 -0500

    complete challenge 14

--- 14_compact/compact.py
+++ 14_compact/compact.py
@@ -4,3 +4,7 @@ def compact(lst):
         >>> compact([0, 1, 2, '', [], False, (), None, 'All done'])
         [1, 2, 'All done']
     """
+
+    true_list = [value for value in lst if value]
+
+    return true_list
