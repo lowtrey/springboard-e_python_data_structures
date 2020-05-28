a6bf389f9ab6e4af38ae912b6af2ed494615bd46
Q. Price <priceqp@gmail.com>
2020-05-28 18:38:16 -0500

    complete challenge 35

--- 35_two_oldest_ages/two_oldest_ages.py
+++ 35_two_oldest_ages/two_oldest_ages.py
@@ -21,3 +21,9 @@ def two_oldest_ages(ages):
     # you may find it helpful to research the `sorted(iter)` function, which
     # can take *any* type of list-like-thing, and returns a new, sorted list
     # from it.
+
+    ages = list(dict.fromkeys(ages))
+
+    sorted_ages = sorted(ages)
+
+    return (sorted_ages[-2], sorted_ages[-1])
