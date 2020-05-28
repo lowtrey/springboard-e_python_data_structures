d4b7e95e0b31413ae15b2e5c16c78ef5335b61e4
Q. Price <priceqp@gmail.com>
2020-05-28 14:07:55 -0500

    complete challenge 26

--- 26_vowel_count/vowel_count.py
+++ 26_vowel_count/vowel_count.py
@@ -3,7 +3,25 @@ def vowel_count(phrase):
 
         >>> vowel_count('rithm school')
         {'i': 1, 'o': 2}
-        
+
         >>> vowel_count('HOW ARE YOU? i am great!') 
         {'o': 2, 'a': 3, 'e': 2, 'u': 1, 'i': 1}
     """
+
+    result = {}
+
+    for letter in phrase:
+
+        lowercase_letter = letter.lower()
+
+        if "aeiou".find(lowercase_letter) != -1:
+
+            if lowercase_letter in result:
+
+                result[lowercase_letter] += 1
+
+            else:
+
+                result[lowercase_letter] = 1
+
+    return result
