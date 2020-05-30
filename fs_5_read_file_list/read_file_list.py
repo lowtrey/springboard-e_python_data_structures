57378a33b0fde0b0f30bd058b2928d5ecc8731b1
Q. Price <priceqp@gmail.com>
2020-05-29 23:27:02 -0500

    complete fs 5

--- fs_5_read_file_list/read_file_list.py
+++ fs_5_read_file_list/read_file_list.py
@@ -19,3 +19,20 @@ def read_file_list(filename):
     # hint: when you read lines of files, there will be a "newline"
     # (end-of-line character) at the end of each line, and you want to
     # strip that off before you print it. Do some research on that!
+
+    # filename += ".txt"
+
+    import os
+
+    __location__ = os.path.realpath(
+        os.path.join(os.getcwd(), os.path.dirname(__file__)))
+
+    f = open(os.path.join(__location__, filename))
+
+    lines = f.readlines()
+
+    lines = [line.strip("\n") for line in lines]
+
+    for line in lines:
+
+        print(f"- {line}")
